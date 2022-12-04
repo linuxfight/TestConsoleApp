dc5dfe5827c73558acdc96543343a6b5e04c995e
linuxfight <72513124+linuxfight@users.noreply.github.com>
2022-12-04 17:06:10 +0300

    Initial commit

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int sum = a + b;
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
